7ce4ca603c5009ed539456098dff739525887616
Andrés Rodriguez <andresrodrigueza@hotmail.com>
2025-04-24 18:27:53 -0300

    new: estilos al contador y botones estilo digital

--- src/ui/Form.tsx
+++ src/ui/Form.tsx
@@ -1,7 +1,13 @@
 'use client';
 
 import React, { useState, useEffect } from "react";
-import ReactLoading from "react-loading"; // Importamos la librería
+import ReactLoading from "react-loading";
+import { Orbitron } from "next/font/google";
+
+const orbitron = Orbitron({
+  subsets: ["latin"],
+  weight: "600",
+});
 
 export default function Counter() {
   const [count, setCount] = useState<number | null>(null);
@@ -15,7 +21,7 @@ export default function Counter() {
       if (data.length > 0) {
         setCount(data[0].value);
       }
-      setIsSubmitting(false); // Aseguramos que esto se ejecute después del setTimeout
+      setIsSubmitting(false);
 
     };
 
@@ -42,32 +48,37 @@ export default function Counter() {
   };
 
   return (
+    // BOTON DE RESTAR
     <div className="flex items-center gap-6 my-8">
       <button
         onClick={() => updateCount((count ?? 0) - 1)}
         disabled={isSubmitting}
-        className="bg-red-500 hover:bg-red-600 text-white rounded-full px-6 py-3 text-2xl"
+        className="font-mono bg-zinc-800 text-gray-100 px-6 py-3 text-3xl rounded-full shadow-md hover:bg-zinc-700 hover:shadow-lg transition-all"
       >
         –
       </button>
 
-      <div className="text-6xl font-bold flex items-center justify-center">
-        {count !== null ? (
-          count
-        ) : (
-          <ReactLoading
-            type="spin"
-            color="#6b7280"
-            height={50}
-            width={50} 
-          />
-        )}
+      {/* CONTADOR */}
+      <div className="w-40 h-40 border-8 border-gray-400 rounded-full flex items-center justify-center">
+        <div id="result" className={`hits ${orbitron.className}`}>
+          {count !== null ? (
+            count
+          ) : (
+            <ReactLoading
+              type="spin"
+              color="#6b7280"
+              height={50}
+              width={50}
+            />
+          )}
+        </div>
       </div>
 
+      {/* BOTON DE SUMAR */}
       <button
         onClick={() => updateCount((count ?? 0) + 1)}
         disabled={isSubmitting}
-        className="bg-green-500 hover:bg-green-600 text-white rounded-full px-6 py-3 text-2xl"
+        className="font-mono bg-zinc-800 text-gray-100 px-6 py-3 text-3xl rounded-full shadow-md hover:bg-zinc-700 hover:shadow-lg transition-all"
       >
         +
       </button>
